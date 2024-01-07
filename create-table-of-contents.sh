54706ff8fe1b90a49f14c9b50be8779fc20830cf
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 15:48:23 -0600

    Modify to processed_repository_name variable

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -3,7 +3,7 @@
 user_name=$(git config --list | grep user.name | cut -f 2 -d "=")
 user_url=$(echo "https://github.com/${user_name}")
 repository_name=$(pwd | rev | cut -d '/' -f 1 | rev)
-PROCESSED_REPOSITORY_NAME=`echo ${repository_name} | tr '[A-Z]' '[a-z]' | tr ' ' '-'`
+processed_repository_name=$(echo ${repository_name} | tr '[A-Z]' '[a-z]' | tr ' ' '-')
 PROCESSED_RNWS=`echo ${repository_name} | tr '[A-Z]' '[a-z]' | tr -d ' '`
 echo "# [go back to Overview](${user_name})" > README.md
 echo "" >> README.md
@@ -22,13 +22,13 @@ do
     gh issue create --title "$INDEX $LINE" --body "$INDEX $LINE"
 done
 echo "" >> README.md
-echo "# [${repository_name}](${user_url}/$PROCESSED_RNWS#$PROCESSED_REPOSITORY_NAME---content)" >> README.md
+echo "# [${repository_name}](${user_url}/$PROCESSED_RNWS#${processed_repository_name}---content)" >> README.md
 echo "" >> README.md
 INDEX=0
 cat $1 | while read LINE
 do 
     let INDEX=$INDEX+1
-    ENUMERATED_LINE=`echo "# $INDEX. [$LINE](${user_url}/$PROCESSED_RNWS#$PROCESSED_REPOSITORY_NAME---content)"` 
+    ENUMERATED_LINE=`echo "# $INDEX. [$LINE](${user_url}/$PROCESSED_RNWS#${processed_repository_name}---content)"` 
     echo "$ENUMERATED_LINE"
     echo "$ENUMERATED_LINE" >> README.md
 done
