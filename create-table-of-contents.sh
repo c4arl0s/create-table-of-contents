7231e5ac8298847f55283e0b375a43a30a65bf8b
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 16:32:28 -0600

    Fix do while loop style

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -10,25 +10,23 @@ echo "" >> README.md
 echo "# [${repository_name} - Content](${user_url}/${processed_rnws}#go-back-to-overview)" >> README.md
 echo "" >> README.md
 index=0
-cat $1 | while read line
-do 
-    let index=${index}+1
-    enumerated_line=$(echo "${index}. [ ] [${index}. ${line}]")
-    master_line=$(echo "#${index}-${line}" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@')
-    final_line=$(echo "${enumerated_line}(${user_url}/${processed_rnws}${master_line})")
-    # replace white spaces with -, replace upper case with lower case, remove ('), \47 is the octal value of it (')
-    echo ${final_line}
-    echo "${final_line}" >> README.md
-    gh issue create --title "${index} ${line}" --body "${index} ${line}"
+cat $1 | while read line; do 
+  let index=${index}+1
+  enumerated_line=$(echo "${index}. [ ] [${index}. ${line}]")
+  master_line=$(echo "#${index}-${line}" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@')
+  final_line=$(echo "${enumerated_line}(${user_url}/${processed_rnws}${master_line})")
+  # replace white spaces with -, replace upper case with lower case, remove ('), \47 is the octal value of it (')
+  echo ${final_line}
+  echo "${final_line}" >> README.md
+  gh issue create --title "${index} ${line}" --body "${index} ${line}"
 done
 echo "" >> README.md
 echo "# [${repository_name}](${user_url}/${processed_rnws}#${processed_repository_name}---content)" >> README.md
 echo "" >> README.md
 index=0
-cat $1 | while read line
-do 
-    let index=${index}+1
-    enumerated_line=$(echo "# ${index}. [${line}](${user_url}/${processed_rnws}#${processed_repository_name}---content)")
-    echo "${enumerated_line}"
-    echo "${enumerated_line}" >> README.md
+cat $1 | while read line; do
+  let index=${index}+1
+  enumerated_line=$(echo "# ${index}. [${line}](${user_url}/${processed_rnws}#${processed_repository_name}---content)")
+  echo "${enumerated_line}"
+  echo "${enumerated_line}" >> README.md
 done
