0eda6f8d7a4298bbe11b02ac2f7c53af2541eba8
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 16:21:32 -0600

    Modify to final_line variable

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -15,10 +15,10 @@ do
     let index=${index}+1
     enumerated_line=$(echo "${index}. [ ] [${index}. ${line}]")
     master_line=$(echo "#${index}-${line}" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@')
-    FINAL_LINE=`echo "${enumerated_line}(${user_url}/${processed_rnws}${master_line})"`
+    final_line=$(echo "${enumerated_line}(${user_url}/${processed_rnws}${master_line})")
     # replace white spaces with -, replace upper case with lower case, remove ('), \47 is the octal value of it (')
-    echo $FINAL_LINE
-    echo "$FINAL_LINE" >> README.md
+    echo ${final_line}
+    echo "${final_line}" >> README.md
     gh issue create --title "${index} ${line}" --body "${index} ${line}"
 done
 echo "" >> README.md
