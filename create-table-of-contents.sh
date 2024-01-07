361097456d22d6a2ab16b3f5093e45c58c613d5c
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 15:58:09 -0600

    Modify to index variable

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -9,26 +9,26 @@ echo "# [go back to Overview](${user_name})" > README.md
 echo "" >> README.md
 echo "# [${repository_name} - Content](${user_url}/${processed_rnws}#go-back-to-overview)" >> README.md
 echo "" >> README.md
-INDEX=0
+index=0
 cat $1 | while read LINE
 do 
-    let INDEX=$INDEX+1
-    ENUMERATED_LINE=`echo "$INDEX. [ ] [$INDEX. $LINE]"`
-    MASTER_LINE=`echo "#$INDEX-$LINE" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@'`
+    let index=${index}+1
+    ENUMERATED_LINE=`echo "${index}. [ ] [${index}. $LINE]"`
+    MASTER_LINE=`echo "#${index}-$LINE" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@'`
     FINAL_LINE=`echo "$ENUMERATED_LINE(${user_url}/${processed_rnws}$MASTER_LINE)"`
     # replace white spaces with -, replace upper case with lower case, remove ('), \47 is the octal value of it (')
     echo $FINAL_LINE
     echo "$FINAL_LINE" >> README.md
-    gh issue create --title "$INDEX $LINE" --body "$INDEX $LINE"
+    gh issue create --title "${index} $LINE" --body "${index} $LINE"
 done
 echo "" >> README.md
 echo "# [${repository_name}](${user_url}/${processed_rnws}#${processed_repository_name}---content)" >> README.md
 echo "" >> README.md
-INDEX=0
+index=0
 cat $1 | while read LINE
 do 
-    let INDEX=$INDEX+1
-    ENUMERATED_LINE=`echo "# $INDEX. [$LINE](${user_url}/${processed_rnws}#${processed_repository_name}---content)"` 
+    let index=${index}+1
+    ENUMERATED_LINE=`echo "# ${index}. [$LINE](${user_url}/${processed_rnws}#${processed_repository_name}---content)"` 
     echo "$ENUMERATED_LINE"
     echo "$ENUMERATED_LINE" >> README.md
 done
