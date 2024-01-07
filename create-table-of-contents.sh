0aaae18405770722154c175792542e1812769061
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 15:43:44 -0600

    Modify to repository_name variable

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -2,12 +2,12 @@
 
 user_name=$(git config --list | grep user.name | cut -f 2 -d "=")
 user_url=$(echo "https://github.com/${user_name}")
-REPOSITORY_NAME=`pwd | rev | cut -d '/' -f 1 | rev`
-PROCESSED_REPOSITORY_NAME=`echo $REPOSITORY_NAME | tr '[A-Z]' '[a-z]' | tr ' ' '-'`
-PROCESSED_RNWS=`echo $REPOSITORY_NAME | tr '[A-Z]' '[a-z]' | tr -d ' '`
+repository_name=$(pwd | rev | cut -d '/' -f 1 | rev)
+PROCESSED_REPOSITORY_NAME=`echo ${repository_name} | tr '[A-Z]' '[a-z]' | tr ' ' '-'`
+PROCESSED_RNWS=`echo ${repository_name} | tr '[A-Z]' '[a-z]' | tr -d ' '`
 echo "# [go back to Overview](${user_name})" > README.md
 echo "" >> README.md
-echo "# [$REPOSITORY_NAME - Content](${user_url}/$PROCESSED_RNWS#go-back-to-overview)" >> README.md
+echo "# [${repository_name} - Content](${user_url}/$PROCESSED_RNWS#go-back-to-overview)" >> README.md
 echo "" >> README.md
 INDEX=0
 cat $1 | while read LINE
@@ -22,7 +22,7 @@ do
     gh issue create --title "$INDEX $LINE" --body "$INDEX $LINE"
 done
 echo "" >> README.md
-echo "# [$REPOSITORY_NAME](${user_url}/$PROCESSED_RNWS#$PROCESSED_REPOSITORY_NAME---content)" >> README.md
+echo "# [${repository_name}](${user_url}/$PROCESSED_RNWS#$PROCESSED_REPOSITORY_NAME---content)" >> README.md
 echo "" >> README.md
 INDEX=0
 cat $1 | while read LINE
