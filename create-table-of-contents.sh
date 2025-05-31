668d7c9947c95d1113d28e9266e95757f00cc382
c4arl0s <c.santiago.cruz@gmail.com>
2025-05-31 14:02:34 -0600

    Invert order lines from content.txt

--- create-table-of-contents.sh
+++ create-table-of-contents.sh
@@ -9,20 +9,23 @@ echo "# [go back to Overview](${user_name})" > README.md
 echo "" >> README.md
 echo "# [${repository_name} - Content](${user_url}/${processed_rnws}#go-back-to-overview)" >> README.md
 echo "" >> README.md
-index=0
-cat $1 | while read line; do 
-  let index=${index}+1
+
+index=$(wc -l < $1 | xargs)
+cat $1 | tac | while read line; do 
   enumerated_line=$(echo "${index}. [ ] [${index}. ${line}]")
   master_line=$(echo "#${index}-${line}" | tr ' ' '-' | tr -d '.'| tr -d ']()'  | tr -d '[' | tr -d ':' | tr -d '\47' | tr -d '>' | tr -d ',' | tr -d '/' | tr -d '\46' | tr -d '$' | tr -d ';' | tr -d '|' | tr -d '\302' | tr -d '\251' | tr -d '\303' | tr -d '\140' | tr -d '’' | tr -d '?' | tr -d '!'| tr -d '%' | tr -d '@')
   final_line=$(echo "${enumerated_line}(${user_url}/${processed_rnws}${master_line})")
-  # replace white spaces with -, replace upper case with lower case, remove ('), \47 is the octal value of it (')
   echo ${final_line}
-  echo "${final_line}" >> README.md
+  echo "${final_line}" >> /private/var/tmp/README.md
   gh issue create --title "${index} ${line}" --body "${index} ${line}"
+  let index=${index}-1
 done
+
+cat /private/var/tmp/README.md | tac >> README.md
 echo "" >> README.md
 echo "# [${repository_name}](${user_url}/${processed_rnws}#${processed_repository_name}---content)" >> README.md
 echo "" >> README.md
+
 index=0
 cat $1 | while read line; do
   let index=${index}+1
@@ -30,3 +33,5 @@ cat $1 | while read line; do
   echo "${enumerated_line}"
   echo "${enumerated_line}" >> README.md
 done
+
+rm /private/var/tmp/README.md
